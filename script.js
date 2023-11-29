4b74c58c814c7821e416581449d8f92e989f491c
gianlucanied <gianluca.nieddu96@gmail.com>
2023-11-29 12:11:44 +0100

    Add: function random number

--- script.js
+++ script.js
@@ -53,3 +53,32 @@ function showContainer() {
 
 
 startButton.addEventListener("click", showContainer);
+
+// Creiamo un Array vuoto
+const numberRandom = [];
+
+// Genero numeri random
+while (numberRandom.length < 16) {
+    //Inserisco solo se il numero non è già presente nell'array
+    let numeroCasuale = genRandomNumMinMax(1, 100);
+
+    let cerca = presenteInArray(numberRandom, numeroCasuale);
+    if (cerca == false) {
+      numberRandom.push(numeroCasuale);
+    }
+  }
+  console.log("Numeri random " + numberRandom);
+
+
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */
+
+
+// Creiamo una funzione che ci generi 16 numeri casuali 
+function genRandomNumMinMax(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
